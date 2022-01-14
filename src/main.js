bbdcb2edffd9391b6b303fc4ac60495040bbaf47
donggoolosori <kdj6551@naver.com>
2022-01-14 22:01:16 +0900

    jsx 사용해서 dom 생성

---
+++ src/main.js
@@ -0,0 +1,29 @@
+/** @jsx h */
+function h(type, props, ...children) {
+  return { type, props, children: children.flat() };
+}
+
+const vm = (
+  <div id="app">
+    <ul>
+      <li>
+        <input type="checkbox" class="toggle" />
+        todo list item 1<button class="remove">삭제</button>
+      </li>
+      <li class="completed">
+        <input type="checkbox" class="toggle" checked />
+        todo list item 2<button class="remove">삭제</button>
+      </li>
+    </ul>
+    <form>
+      <input type="text" />
+      <button type="submit">추가</button>
+    </form>
+  </div>
+);
+
+const $root = document.body.querySelector('#root');
+
+$root.innerHTML = `
+  <pre>${JSON.stringify(vm, null, 2)}</pre>
+`;
