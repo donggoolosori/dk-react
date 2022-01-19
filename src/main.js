1826c5e4c30c1bb3b6b394aeae91097960c8160f
donggoolosori <kdj6551@naver.com>
2022-01-19 14:11:50 +0900

    feat: 상태 추가

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
-/** @jsx h */
-function h(type, props, ...children) {
-  return { type, props, children: children.flat() };
-}
+const state = [
+  { id: 1, completed: false, content: 'todo list item 1' },
+  { id: 2, completed: true, content: 'todo list item 2' },
+];
 
 const vm = (
   <div id="app">
     <ul>
-      <li>
-        <input type="checkbox" class="toggle" />
-        todo list item 1<button class="remove">삭제</button>
-      </li>
-      <li class="completed">
-        <input type="checkbox" class="toggle" checked />
-        todo list item 2<button class="remove">삭제</button>
-      </li>
+      {state.map(({ completed, content }) => {
+        <li class={completed ? 'completed' : null}>
+          <input type="checkbox" class="toggle" checked={completed} />
+          {content}
+          <button class="remove">삭제</button>
+        </li>;
+      })}
     </ul>
     <form>
       <input type="text" />
