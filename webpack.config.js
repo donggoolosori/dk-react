40b22d7d6a0239ca9215767238587174f1ab8509
donggoolosori <kdj6551@naver.com>
2022-02-18 21:16:36 +0900

    chore: webpack 설치 및 환경설정

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+};
