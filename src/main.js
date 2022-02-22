102d7838fb57586e8ee50353a3dba9bde9a4a30e
donggoolosori <kdj6551@naver.com>
2022-02-22 22:01:18 +0900

    chore: render함수 사용

--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
 import DkReact from '.';
 
-const state = [
-  { id: 1, completed: false, content: 'todo list item 1' },
-  { id: 2, completed: true, content: 'todo list item 2' },
-];
-
 /** @jsx DkReact.createElement */
-const virtualDom = (
+const element = (
   <div id="app">
-    <ul>
-      {state.map(({ completed, content }) => (
-        <li className={completed ? 'completed' : null}>
-          <input type="checkbox" className="toggle" checked={completed} />
-          {content}
-          <button className="remove">삭제</button>
-        </li>
-      ))}
-    </ul>
     <form>
       <input type="text" />
       <button type="submit">추가</button>
@@ -24,4 +10,5 @@ const virtualDom = (
   </div>
 );
 
-console.log(virtualDom);
+const container = document.getElementById('root');
+DkReact.render(element, container);
