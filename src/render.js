0c335d2b8abca143615f6d190ffe7aa75aaae4b6
donggoolosori <kdj6551@naver.com>
2022-02-26 20:59:44 +0900

    feat: Add performUnitOfWork function
    fiber 단위로 작업을 수행하고 다음 작업을 반환하는 함수

--- src/render.js
+++ src/render.js
@@ -1,14 +1,73 @@
+import createDom from './createDom';
+
 export default function render(element, container) {
-  const dom =
-    element.type === 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(element.type);
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
 
-  Object.keys(element.props)
-    .filter((key) => key !== 'children')
-    .forEach((key) => (dom[key] = element.props[key]));
+let nextUnitOfWork = null;
 
-  element.props.children.forEach((child) => render(child, dom));
+function workLoop(deadline) {
+  let noTime = false;
 
-  container.appendChild(dom);
+  while (nextUnitOfWork && !noTime) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+
+    noTime = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const { children } = fiber.props;
+
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < children.length) {
+    const { type, props, parent } = children[index];
+
+    const newFiber = {
+      type,
+      props,
+      parent,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
