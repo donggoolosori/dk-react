e47e1ea712393f653dc86c38ef8a8013d00a9eff
donggoolosori <kdj6551@naver.com>
2022-03-02 17:13:58 +0900

    feat: oldFiber 갱신하기

--- src/render.js
+++ src/render.js
@@ -1,4 +1,5 @@
 import createDom from './createDom';
+import updateDom from './updateDom';
 
 export default function render(element, container) {
   wipRoot = {
@@ -11,45 +12,6 @@ export default function render(element, container) {
 
   deletions = [];
   nextUnitOfWork = wipRoot;
-}
-
-const isEvent = (key) => key.startsWith('on');
-const isProperty = (key) => key !== 'children' && !isEvent(key);
-const isNew = (prev, next) => (key) => prev[key] !== next[key];
-const isGone = (prev, next) => (key) => !(key in next);
-
-function updateDom(dom, prevProps, nextProps) {
-  // Remove old or changed event listeners
-  Object.keys(prevProps)
-    .filter(isEvent)
-    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
-    .forEach((name) => {
-      const eventType = name.toLowerCase().substring(2);
-
-      dom.removeEventListener(eventType, prevProps[name]);
-    });
-
-  // Add event listeners
-  Object.keys(nextProps)
-    .filter(isEvent)
-    .filter(isNew(prevProps, nextProps))
-    .forEach((name) => {
-      const eventType = name.toLowerCase().substring(2);
-
-      dom.addEventListener(eventType, nextProps[name]);
-    });
-
-  // Remove old properties
-  Object.keys(prevProps)
-    .filter(isProperty)
-    .filter(isGone(prevProps, nextProps))
-    .forEach((name) => (dom[name] = ''));
-
-  // Add or Change new properties
-  Object.keys(nextProps)
-    .filter(isProperty)
-    .filter(isNew(prevProps, nextProps))
-    .forEach((name) => (dom[name] = nextProps[name]));
 }
 
 function commitRoot() {
@@ -158,12 +120,9 @@ function reconcileChildren(wipFiber, elements) {
       (oldFiber.effectTag = 'DELETION'), deletions.push(oldFiber);
     }
 
-    const newFiber = {
-      type,
-      props,
-      parent: wipFiber,
-      dom: null,
-    };
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
 
     if (index === 0) {
       wipFiber.child = newFiber;
