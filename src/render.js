eb88f79b2e13d7e5b18d00b90389fd92ee855271
donggoolosori <kdj6551@naver.com>
2022-03-06 23:35:35 +0900

    feat: useState 함수 구현

--- src/render.js
+++ src/render.js
@@ -107,7 +107,36 @@ function updateFunctionComponent(fiber) {
   reconcileChildren(fiber, children);
 }
 
-function useState(initial) {}
+function useState(initial) {
+  const oldHook =
+    wipFiber.alternate &&
+    wipFiber.alternate.hooks &&
+    wipFiber.alternate.hooks[hookIndex];
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  };
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach((action) => {
+    hook.state = action(hook.state);
+  });
+
+  const setState = (action) => {
+    hook.queue.push(action);
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    };
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  };
+
+  wipFiber.hooks.push(hook);
+  hookIndex++;
+  return [hook.state, setState];
+}
 
 function updateHostComponent(fiber) {
   if (!fiber.dom) {
